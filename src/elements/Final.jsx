8231f562e199cc17479270f74c37efcc6d992c9e
Anyi Lozano <anyisithalozano12@gmail.com>
2022-08-04 17:40:54 -0500

    gnbnbvn

--- src/elements/Final.jsx
+++ src/elements/Final.jsx
@@ -8,9 +8,9 @@ export const Final = ({ restarGame}) => {
   return (
    <div className="end animate__animated animate__fadeIn">
         <div className="final-table">
-          <a href='https://instagram.com/sospechar_nofalla?igshid=YmMyMTA2M2Y=' target="_blank" style={{ cursor: 'pointer',  position: "absolute", bottom: "23%", marginLeft: "27%", height: "37%", width: "7%" }}>color</a>
-          <a href='https://www.facebook.com/sospecharnofalla/' target="_blank" style={{ cursor: 'pointer',  position: "absolute", bottom: "23%", marginLeft: "37%", height: "37%", width: "7%" }}>color</a>
-          <a href='https://web-sospechar-para-todos.sospecharnofalla.com/contenido-para-todos' target="_blank" style={{ cursor: 'pointer',  position: "absolute", bottom: "23%", marginLeft: "46%", height: "37%", width: "7%" }}>color</a>
+          <a href='https://instagram.com/sospechar_nofalla?igshid=YmMyMTA2M2Y=' target="_blank" style={{ cursor: 'pointer',  position: "absolute", bottom: "23%", marginLeft: "27%", height: "37%", width: "7%" }}></a>
+          <a href='https://www.facebook.com/sospecharnofalla/' target="_blank" style={{ cursor: 'pointer',  position: "absolute", bottom: "23%", marginLeft: "37%", height: "37%", width: "7%" }}></a>
+          <a href='https://web-sospechar-para-todos.sospecharnofalla.com/contenido-para-todos' target="_blank" style={{ cursor: 'pointer',  position: "absolute", bottom: "23%", marginLeft: "46%", height: "37%", width: "7%" }}></a>
           <div className="content-end">
              {/* <div className='repetir' onClick={()=> restarGame()}></div> */}
              <a className='repetir' href="./"></a>
